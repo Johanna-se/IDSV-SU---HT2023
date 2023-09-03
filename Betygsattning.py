7523a631ab14d728bd8866611001812663b0acf7
Johanna Petersson <jojanna.petersson@hotmail.com>
2023-09-03 15:45:32 +0200

    Övningar

---
+++ Betygsattning.py
@@ -0,0 +1,16 @@
+Poang = int(input("Vilken poäng fick du?: "))
+
+if Poang >= 34:
+    print("Du fick ett A.")
+elif Poang >= 32:
+    print("Du fick ett B.")
+elif Poang >= 28:
+    print("Du fick ett C.")
+elif Poang >= 24:
+    print("Du fick ett D.")
+elif Poang >= 20:
+    print("Du fick ett E.")
+elif Poang >= 18:
+    print("Du fick ett Fx.")
+else:
+    print("Du fick ett F.")
