908c0cb0b5b5d4fa78a7708e8840c55337f06d6f
Johanna Petersson <jojanna.petersson@hotmail.com>
2023-09-07 10:37:08 +0200

    övningar föreläsning 6

---
+++ Övningar från föreläsningen/ListorEtt.py
@@ -0,0 +1,33 @@
+talen = []
+
+tal = int(input("Tal: "))
+while tal >= 0:
+    talen += [tal]  #Slår ihop två listro, bättre metod för att göra detta kommer på nästa föreläsning
+    tal = int(input("Tal: "))
+
+kom = input("Kommando: ")
+while kom != "sluta":
+    match kom:
+        case "skriv":
+            for nummer in talen:
+                print(nummer, end = " ")    #kommandot end = " " gör att nästa siffra ej skrivs på en nya rad utan att det blir ett mellanslag istället (följt av nästa siffra i listan)
+            print() #Byt rad efter for-loopen
+        case "summa":
+            summa = 0
+            for nummer in talen:
+                summa += nummer
+            print("Summa =", summa)
+        case "addera":                      
+            pos = int(input("Position: "))
+            if pos < 0 or pos >= len(talen):
+                print("Fel position!")
+            else:
+                talet = int(input("Att addera: "))
+                talen[pos] += talet
+        case "sluta":
+            pass        #betyder att programmet hoppas över.
+        case _: #alla övriga 
+            print("Fel kommando!")
+    kom = input("Kommando: ")
+
+print("Tack och välkommen åter!")
