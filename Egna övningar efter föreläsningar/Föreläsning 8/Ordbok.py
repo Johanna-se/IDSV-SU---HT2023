be68e7ef4af164538e2d385aed24fd14bfac660c
Johanna Petersson <jojanna.petersson@hotmail.com>
2023-09-12 14:04:52 +0200

    Update Ordbok.py

---
+++ Egna övningar efter föreläsningar/Föreläsning 8/Ordbok.py
@@ -0,0 +1,12 @@
+# Skriv ett Python-program som fungerar som en liten svensk-engelsk ordbok.
+# Skapa i programmet två listor med strängar: en med svenska ord och den andra med
+# motsvarande engelska ord. Var noga med att de svenska orden och motsvarande engelska ord
+# ligger på samma position i var sin lista. Programmet ska sedan i en loop läsa in ett svenskt ord
+# och skriva ut motsvarande engelska ord. Om det inlästa svenska ordet inte finns i svenska
+# listan ska ett felmeddelande skrivas ut. Programmet ska avslutas när användaren matat in
+# ordet ”sluta” som svenskt ord. Det ska inte spela någon roll om användaren matar in de
+# svenska orden med versaler, gemena eller en blandning av dessa.
+# Lägg inte ner mycket tid på att hitta på de svenska och engelska orden, de behöver inte vara
+# många – strukturen i programmet blir likadant ändå.
+# Obs! att det finns bättre lösningar än två listor för att lösa detta problem, men vi har inte tagit
+# upp dem än, och övningen är en bra övning på listor.
