8f3595220d28d9b8169d0850d6740c3a82ee9040
Johanna Petersson <jojanna.petersson@hotmail.com>
2023-09-14 10:38:00 +0200

    Create BMIRevisited.py

---
+++ Egna övningar efter föreläsningar/Föreläsning 11/BMIRevisited.py
@@ -0,0 +1,26 @@
+# Tillsammans med den här texten en lösning på övningsuppgiften med BMI-beräkning.
+# Den börjar med två nästan likadana kodsnuttar för inläsning av vikt resp. längd med
+# rimlighetskontroll.
+# Skriv om programmet med en funktion som gör inläsningen istället, så att man slipper
+# upprepning av kod .
+# Funktionen ska alltså läsa in ett float-tal och göra en rimlighetskontroll av talet.
+# Funktionen ska returnera det inlästa talet.
+# De värden som skiljer de två kodsnuttarna åt får bli parametrar till funktionen
+
+# Från föreläsning 4a:
+# Utöka BMI-uppgiften från övningar efter F2 med att även skriva ut viktklass enligt följande
+# tabell:
+# Mindre än 18.5    underviktig
+# 18.5 - 24.9       Normalviktig
+# 25.0 - 29.9       Överviktig
+# Över 30           fetma
+
+#Från föreläsning 2:
+# Skriv ett Python-program som frågar användaren om hens vikt i kg och hens längd i meter och
+# som beräknar och skriver ut hens BMI (Body Mass Index).
+# BMI räknas ut som vikten dividerad med längden i kvadrat:
+# bmi = vikt / langd2
+# Exempel på programdialogen (användarens inmatning i svart):
+# Vikt: 83.2
+# Längd: 1.76
+# BMI = 26.859504132231407
